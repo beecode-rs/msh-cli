49f9a53c62334cd70956aa08360617676a935768
milos <milos.bugarinovic@gmail.com>
2020-11-28 16:33:24 +0100

    refactor: add npm global code

--- src/service/npm-service.ts
+++ src/service/npm-service.ts
@@ -1,3 +1,7 @@
+import cVer from 'compare-versions'
+import stringify from 'fast-json-stable-stringify'
+import fs from 'fs'
+import * as path from 'path'
 import { PrintStdMessage, cliService } from 'src/service'
 import { config, logger } from 'src/util'
 
@@ -22,6 +26,56 @@ export const npmService = {
     cliService.printStdMessage(...results)
   },
   global: (): void => {
-    throw new Error('Not implemented yet')
+    const gDeps = {}
+    const gDepsNewer = {}
+    const gDepsOlder = {}
+
+    for (const project of config.projects) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const packageJs = require(path.join(process.cwd(), `${project}/package.json`))
+      const allDeps = { ...packageJs.dependencies, ...packageJs.devDependencies }
+      for (const key of Object.keys(allDeps)) {
+        gDeps[key] = gDeps[key] || {}
+        gDeps[key][allDeps[key]] = gDeps[key][allDeps[key]] || []
+        gDeps[key][allDeps[key]].push(project)
+      }
+    }
+
+    for (const key of Object.keys(gDeps)) {
+      if (Object.keys(gDeps[key]).length > 1) {
+        const sortedVersionsAsc = Object.keys(gDeps[key]).sort(cVer)
+        gDepsNewer[key] = sortedVersionsAsc.pop()
+
+        for (const oldVer of sortedVersionsAsc) {
+          gDepsOlder[key] = gDepsOlder[key] || {}
+          gDepsOlder[key][oldVer] = gDeps[key][oldVer]
+        }
+      } else {
+        gDepsNewer[key] = Object.keys(gDeps[key])[0]
+      }
+    }
+
+    // revert gDepsOlder result to print by project
+    const gDepsReverted = {}
+    for (const pack of Object.keys(gDepsOlder)) {
+      const versions = gDepsOlder[pack]
+      for (const ver of Object.keys(versions)) {
+        const projs = versions[ver]
+        for (const proj of projs) {
+          gDepsReverted[proj] = gDepsReverted[proj] || {}
+          gDepsReverted[proj][pack] = ver
+        }
+      }
+    }
+    logger.debug('Old Deps (by project):')
+    logger.debug(JSON.stringify(gDepsReverted, null, 2))
+    logger.debug('Old Deps (by package):')
+    logger.debug(JSON.stringify(gDepsOlder, null, 2))
+
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const globalPackageJs = require(path.join(process.cwd(), `package.json`))
+    globalPackageJs.dependencies = gDepsNewer
+
+    fs.writeFileSync('package.json', JSON.stringify(JSON.parse(stringify(globalPackageJs)), null, 4), 'utf8')
   },
 }
