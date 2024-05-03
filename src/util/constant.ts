88d9d93d803a2dbd27a2fc5f9fbda7ab45dbeae8
milosbugarinovic <milos.bugarinovic@gmail.com>
2024-05-03 23:05:27 +0200

    fix: update package.json, use v8 for coverage

--- src/util/constant.ts
+++ src/util/constant.ts
@@ -1,5 +1,7 @@
-import packageJson from '../../package.json' assert { type: 'json' }
 import { singletonPattern } from '@beecode/msh-util/singleton/pattern'
+
+// @ts-expect-error
+import packageJson from '#packageJson' assert { type: 'json' }
 
 export const constant = singletonPattern(() =>
 	Object.freeze({
